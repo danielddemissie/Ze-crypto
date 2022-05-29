49036f28985bc7998ba732ae5d5ca799911f58f4
daniel <danielddemissie@gmail.com>
2022-05-29 12:07:01 +0300

    change scale

--- src/scale.js
+++ src/scale.js
@@ -1,5 +1,5 @@
 const increaseScale = (bot) => {
-  return bot.action('I_5', (ctx) => {
+  return bot.action('I_5', async(ctx) => {
     if (scale >= 40) {
       ctx.reply(
         'The scale is too much .\nKnow that you will not recive updates quickly.'
@@ -9,6 +9,7 @@ const increaseScale = (bot) => {
       ctx.reply('The scale is increased by 5.');
       scale += 5;
     }
+    await ctx.answerCbQuery();
   });
 };
 const decreaseScale = (bot) => {
@@ -20,6 +21,7 @@ const decreaseScale = (bot) => {
       ctx.reply('The scale is decreased by 5.');
       scale += 5;
     }
+    await ctx.answerCbQuery();
   });
 };
 module.exports = {
